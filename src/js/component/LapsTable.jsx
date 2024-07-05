105864f9f807cf70d3a03d0ecb9df4a7285d6774
EberG25 <168913317+EberG25@users.noreply.github.com>
2024-07-05 13:01:19 +0000

    finsh-last

--- src/js/component/LapsTable.jsx
+++ src/js/component/LapsTable.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const LapsTable = ({ laps }) => {
   return (
     <>
-    <table className="w-full text-center mt-4 text-green-900">
+    <table className="w-full text-center mt-4 text-green-900 border-t-4 border-b-2 border-l-2 border-r-2 bg-green-100">
       <thead>
         <tr>
           <th>Lap</th>
@@ -13,7 +13,7 @@ export const LapsTable = ({ laps }) => {
       </thead>
       <tbody>
         {laps.map((lap, index) => (
-          <tr key={index}>
+          <tr key={index} className="bg-green-200 text-xl">
             <td>{index + 1}</td>
             <td>{lap.counter}</td>
             <td>{lap.difference}</td>
